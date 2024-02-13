558102508450f26d009ccf991f9ad782d85af52f
MounirIssa <maliissa@ucsd.edu>
2024-02-12 21:30:51 -0800

    new changes

--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Task.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Task.java
@@ -7,34 +7,33 @@ import androidx.annotation.Nullable;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Flashcard implements Serializable {
+public class Task implements Serializable {
     private final @Nullable Integer id;
     private final @NonNull String task;
     private final @NonNull Integer sortOrder;
-    private final @NonNull boolean checkedOff;
+    private final @NonNull Boolean checkedOff;
 
-    public Flashcard(@Nullable Integer id, @NonNull String task, int sortOrder, boolean checkedOff) {
+    public Task(@Nullable Integer id, @NonNull String task, int sortOrder, boolean checkedOff) {
         this.id = id;
         this.task = task;
         this.sortOrder = sortOrder;
-        this.checkedOff = false;
+        this.checkedOff = checkedOff;
     }
 
     public @Nullable Integer id() { return id; }
     public int sortOrder() { return sortOrder; }
-    public Flashcard withSortOrder(int sortOrder) { return new Flashcard(this.id, this.task, sortOrder, checkedOff);  }
-    public Flashcard withId(int id) { return new Flashcard(id, this.task, this.sortOrder, checkedOff); }
-    public Flashcard withCheckOff(boolean checkOff) { return new Flashcard(id, task, sortOrder, checkOff); }
+    public Task withSortOrder(int sortOrder) { return new Task(this.id, this.task, sortOrder, checkedOff);  }
+    public Task withId(int id) { return new Task(id, this.task, this.sortOrder, checkedOff); }
+    public Task withCheckOff(boolean checkOff) { return new Task(id, task, sortOrder, checkOff); }
 
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Flashcard flashcard = (Flashcard) o;
-        return Objects.equals(id, flashcard.id) && Objects.equals(task, flashcard.task) && Objects.equals(sortOrder, flashcard.sortOrder);
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(this.task, task.task) && Objects.equals(sortOrder, task.sortOrder);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(id, task, sortOrder);
---
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/TasksRepository.java
@@ -0,0 +1,23 @@
+package edu.ucsd.cse110.successorator.lib.domain;
+
+import java.util.List;
+
+import edu.ucsd.cse110.successorator.lib.util.Subject;
+
+public interface TasksRepository {
+    Subject<Task> find(int id);
+
+    Subject<List<Task>> findAll();
+
+    void save(Task task);
+
+    void save(List<Task> tasks);
+
+    void remove(int id);
+
+    void append(Task task);
+
+    void prepend(Task task);
+
+    int size();
+}
