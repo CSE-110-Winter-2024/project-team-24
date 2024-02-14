4b92320d7e3b246e9702e2efa3166e57d21d8fcf
MounirIssa <maliissa@ucsd.edu>
2024-02-13 18:42:04 -0800

    implemented tasklistadapter

---
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/TaskListAdapter.java
@@ -0,0 +1,63 @@
+package edu.ucsd.cse110.successorator.ui;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.core.util.Consumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import edu.ucsd.cse110.successorator.app.databinding.ListItemTaskBinding;
+import edu.ucsd.cse110.successorator.app.databinding.ListItemTaskBinding;
+import edu.ucsd.cse110.successorator.lib.domain.Task;
+
+public class TaskListAdapter extends ArrayAdapter<Task> {
+    public TaskListAdapter(Context context, List<Task> tasks) {
+        super(context, 0, new ArrayList<>(tasks));
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        var task = getItem(position);
+        assert task != null;
+
+        ListItemTaskBinding binding;
+        if (convertView != null) {
+            binding = ListItemTaskBinding.bind(convertView);
+        }
+        else {
+            var layoutInflater = LayoutInflater.from(getContext());
+            binding = ListItemTaskBinding.inflate(layoutInflater, parent, false);
+        }
+
+        binding.taskText.setText(task.getTask());
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        var task = getItem(position);
+        assert task != null;
+
+        var id = task.id();
+        assert id != null;
+
+        return id;
+    }
+
+
+}
