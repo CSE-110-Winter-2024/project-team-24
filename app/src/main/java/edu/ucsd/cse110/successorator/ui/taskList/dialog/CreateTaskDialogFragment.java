6480e19e12b66bfc68337d6673c0f2cef8f2ef9c
pink10000 <kyletrinh1223@gmail.com>
2024-03-04 18:44:22 -0800

    [us8a-1] implemented radio group

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/taskList/dialog/CreateTaskDialogFragment.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/taskList/dialog/CreateTaskDialogFragment.java
@@ -34,11 +34,30 @@ public class CreateTaskDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         this.view = FragmentDialogCreateTaskBinding.inflate(getLayoutInflater());
+        // Create the radio button list items
+        String[] items = {"One-Time", "Daily", "Weekly on Tu", "Monthly 3rd Tu", "Yearly on 2/20"};
+        // Set the default checked item (index of items array, in this case 'one-time' is checked by default)
+        int checkedItem = 0;
 
         return new AlertDialog.Builder(getActivity())
                 .setView(view.getRoot())
                 .setTitle("New Task")
-                .setPositiveButton("Create", this::onPositiveButtonClick)
+                // Replace 'setPositiveButton' with 'setSingleChoiceItems' for radio buttons
+                .setSingleChoiceItems(items, checkedItem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Handle the radio button selection
+                    }
+                })
+                // Change button text to "Save" and handle click
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Handle the Save button click
+                        int selectedPosition = ((AlertDialog)dialogInterface).getListView().getCheckedItemPosition();
+                        // Do something with the position
+                    }
+                })
                 .setNegativeButton("Cancel", this::onNegativeButtonClick)
                 .setMessage("Please provide the new task.")
                 .create();
